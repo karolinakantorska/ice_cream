84f4d8ec3a16f0065a89a54f455b212feaf2a0ee
KarolinaSun2Wheel <karolina.kantorska@sun2wheel.ch>
2026-01-21 16:31:54 +0100

    basic routes

---
+++ backend/src/RoutingFunctions.ts
@@ -0,0 +1,90 @@
+import {Response, Request} from "express"
+import mock_ices from "./mock_ices.json"
+import { ApiResponse, IceCream } from "./TS";
+
+export const getIceCreams =(req: Request, res: Response<ApiResponse<IceCream[]>>) => {
+  res
+    .status(200)
+    .json({
+        success: true,
+        results: mock_ices.length,
+        data: 
+            mock_ices
+       });
+}
+
+export const getIceCream = (req: Request<{ id: string }>, res: Response<ApiResponse<IceCream>>) =>{
+    console.log(req.params)
+    const id = req.params.id
+
+    const iceCream = mock_ices.find((ice:IceCream)=> ice.id === id)
+
+    if(!iceCream){
+        return res
+            .status(404)
+            .json({
+            success: false,
+            message:'invalid id'
+        })
+    }
+
+    res
+        .status(200)
+        .json({
+            success: true,
+            data: iceCream
+        })
+}
+
+export const createIceCream = (req: Request<{}, {}, IceCream>, res: Response<ApiResponse<IceCream>>) =>{
+        console.log(req.body)
+        res
+            .status(201)
+            .json({
+                success: true,
+                data:req.body
+            })
+    }
+
+export const updateIceCream = (req: Request<{ id: string },{}, IceCream>, res: Response<ApiResponse<string>>) =>{
+        const id = req.params.id
+
+        const iceCream = mock_ices.find((ice:IceCream)=> ice.id === id)
+
+    if(!iceCream){
+        return res
+            .status(404)
+            .json({
+            success: false,
+            message:'not found'
+        })
+    }
+        res
+            .status(200)
+            .json({
+                success: true,
+                data:id
+
+            })
+    }
+
+export const deleteIceCream = (req: Request<{ id: string }>, res: Response<ApiResponse<string>>) =>{
+        const id = req.params.id
+
+        const iceCream = mock_ices.find((ice:IceCream)=> ice.id === id)
+
+    if(!iceCream){
+        return res
+            .status(404)
+            .json({
+                success: false,
+                message:'not found'
+        })
+    }
+        res
+            .status(200)
+            .json({
+                success: true,
+                message:'Ice cream deleted succesfully.'
+            })
+    }
---
+++ backend/src/TS.ts
@@ -0,0 +1,31 @@
+import {Response, Request} from "express"
+
+interface Recipe {
+  ingrediend_id: string;
+  amount: number;
+}
+
+
+export interface IceCream {
+    id?: string;
+    name: string;
+    shape_id: string;
+    size: number;
+    receipe: Recipe[];
+    description: string;
+    price: number;
+    owner_id: string;
+    loves: number;
+}
+
+// Request
+
+
+// Response
+
+export interface ApiResponse<T> {
+    success: boolean
+    results?:number
+    data?: T
+    message?: string
+}
